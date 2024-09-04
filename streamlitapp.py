6e6a0c1e78d851e4934a508f5a2b0a96bdd35da4
MunazaShakeel <munazashakeel555@gmail.com>
2024-09-04 12:50:04 +0000

    Added

--- streamlitapp.py
+++ streamlitapp.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import streamlit as st
 #Load the updated live modelto be made live
-model = joblib.load("liveModelV1.pkl")
+model = joblib.load("liveModelV1.pk1")
 #Load the data to check accuracy
 data = pd.read_csv('mobile_price_range_data.csv')
 
